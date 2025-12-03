e0ec428df6b025592d6a154bde97dca5764c3470
hdhumanez <hdhumanez@gmail.com>
2025-12-03 17:04:53 -0500

    [BASE] prueba técnica base presentada por Heyner Humanez

--- src/onboarding/onboarding.service.spec.ts
+++ src/onboarding/onboarding.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
 import { OnboardingService } from './onboarding.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
